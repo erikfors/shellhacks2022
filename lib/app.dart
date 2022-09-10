f02af6150eb67b89f9cd1c78e2518908993625df
erikfors <efors003@fiu.edu>
2022-09-10 06:02:04 -0400

    authentication done, folder set up done

---
+++ lib/app.dart
@@ -0,0 +1,56 @@
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shellhacks2022/Presentation/Screens/home_page.dart';
+import 'package:shellhacks2022/Presentation/Screens/login_page.dart';
+import 'package:shellhacks2022/Presentation/Screens/sign_up_page.dart';
+
+import 'Data/Blocs/bloc_app/app_bloc.dart';
+import 'Data/Repositories/authentication_repository.dart';
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+    required AuthenticationRepository authenticationRepository,
+  }) : _authenticationRepository = authenticationRepository;
+
+  final AuthenticationRepository _authenticationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AppBloc(
+          authenticationRepository: _authenticationRepository,
+        ),
+        child: const AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        //theme: theme,
+        home: FlowBuilder<AppStatus>(
+      state: context.select((AppBloc bloc) => bloc.state.status),
+      onGeneratePages: (state, pages) {
+        switch (state) {
+          case AppStatus.unauthenticated:
+            return [
+              const MaterialPage(child: LoginPage()),
+            ];
+          case AppStatus.authenticated:
+            return [
+              const MaterialPage(child: HomePage()),
+            ];
+        }
+      },
+    ));
+  }
+}
