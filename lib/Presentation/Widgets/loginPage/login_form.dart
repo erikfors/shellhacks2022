6b30fb27d14b9ed93522c41ecc9c0c2767be95ce
leo <leohub0103@gmail.com>
2022-09-10 15:20:23 -0400

    java problem fixed

--- lib/Presentation/Widgets/loginPage/login_form.dart
+++ lib/Presentation/Widgets/loginPage/login_form.dart
@@ -133,3 +133,40 @@ class _SignUpButton extends StatelessWidget {
     );
   }
 }
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override 
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>{
+  @override  
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(children: [
+        // Hello Again 
+        Text("Hello everyone"),
+
+
+        //email 
+
+
+        //password
+
+        //sign in button
+
+        //not a member? register now
+
+
+
+
+      ]),
+      ),
+    );
+  }
+}
