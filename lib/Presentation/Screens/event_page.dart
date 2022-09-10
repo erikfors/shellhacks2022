2b63742eea74f9d722145223865f190c2e7cbcbe
erikfors <efors003@fiu.edu>
2022-09-10 18:16:53 -0400

    folder rearange

--- lib/Presentation/Screens/event_page.dart
+++ lib/Presentation/Screens/event_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../Data/Models/event.dart';
 import '../../Data/Models/user.dart';
+import '../Widgets/EventsPage/single_event.dart';
 
 class EventPage extends StatelessWidget {
   EventPage({super.key});
@@ -44,109 +45,7 @@ class EventPage extends StatelessWidget {
         child: ListView.builder(
           itemCount: events.length,
           itemBuilder: (context, index) {
-            return Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(25.0),
-                child: SizedBox(
-                  height: 350,
-                  child: Stack(
-                    children: [
-                      Image.network(
-                        'https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
-                        height: 400,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Colors.black.withOpacity(0),
-                                Colors.black.withOpacity(0.8),
-                              ],
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Colors.white,
-                            foregroundColor: Colors.black,
-                            child: Text(
-                              events[index].participants!.length.toString(),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SizedBox(
-                            height: 70,
-                            width: 70,
-                            child: Card(
-                              child: Center(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      events[index].eventTime!.day.toString(),
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                    Text(
-                                      getMonthName(
-                                          events[index].eventTime!.month),
-                                      style: TextStyle(
-                                        fontSize: 12,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: ListTile(
-                          title: Text(
-                            events[index].title!,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          trailing: Text(
-                            events[index].zipCode.toString(),
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
+            return SingleEvent(eventTime: events[index].eventTime,owner: events[index].owner,participants: events[index].participants,title: events[index].title,zipCode: events[index].zipCode,);
           },
         ),
       ),
@@ -154,34 +53,5 @@ class EventPage extends StatelessWidget {
   }
 }
 
-// get name of month from int
-String getMonthName(int month) {
-  switch (month) {
-    case 1:
-      return 'Jan';
-    case 2:
-      return 'Feb';
-    case 3:
-      return 'Mar';
-    case 4:
-      return 'Apr';
-    case 5:
-      return 'May';
-    case 6:
-      return 'Jun';
-    case 7:
-      return 'Jul';
-    case 8:
-      return 'Aug';
-    case 9:
-      return 'Sep';
-    case 10:
-      return 'Oct';
-    case 11:
-      return 'Nov';
-    case 12:
-      return 'Dec';
-    default:
-      return 'Invalid Month';
-  }
-}
+
+
